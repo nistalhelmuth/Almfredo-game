69b726fa14b93bbdd885fa6b46cbd8f2c98a250a
pablogadhi <pablogadhi@gmail.com>
2018-11-20 22:51:38 -0600

    Composicion del menu principal

---
+++ Assets/Scripts/Menu/MenuCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu
+{
+    public class MenuCharacter : MonoBehaviour
+    {
+
+        public Camera camera;
+
+        private bool started = false;
+
+        void Start()
+        {
+            camera.gameObject.GetComponent<MenuManager>().GoToMainMenu += LookUp;
+        }
+
+        void Update ()
+        {
+            if (started)
+            {
+                Vector3 viewVector = camera.transform.position - transform.position;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(viewVector), 1.5f * Time.deltaTime);
+            }
+        }
+
+        public void LookUp()
+        {
+            started = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public delegate void MenuAction();
+    public event MenuAction GoToMainMenu;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    IEnumerator MainMenu()
+    {
+        yield return new WaitForSeconds(3f);
+        GoToMainMenu();
+    }
+}
